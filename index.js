de01f599c803ab6761054e35c84bf0ced278d8a4
seymurhesenzade <hasanzadeseymur21@gmail.com>
2024-04-25 22:54:57 +0400

    ToDoList

--- index.js
+++ index.js
@@ -1,32 +1,54 @@
 const addBtn = document.getElementById("btn");
 const clearBtn = document.getElementById("clearbtn");
 const toDoContainer = document.getElementById("toDoContainer");
-const inputText= document.getElementById("inputText");
+const inputText = document.getElementById("inputText");
+
 // -------------------------------------------------------
-addBtn.addEventListener("click", function(){
-    let paragraph = document.createElement("p");
-toDoContainer.appendChild(paragraph);
-paragraph.innerHTML=inputText.value;
-inputText.value="";
+addBtn.addEventListener("click", function () {
+  let paragraph = document.createElement("p");
+  toDoContainer.appendChild(paragraph);
+  paragraph.innerHTML = inputText.value;
+  inputText.value = "";
 
+  Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: "Your work has been saved",
+    showConfirmButton: false,
+    timer: 1500,
+  });
 
-paragraph.addEventListener("dblclick", function(){
+  paragraph.addEventListener("dblclick", function () {
     toDoContainer.removeChild(paragraph);
-})
+  });
 });
 
-clearBtn.addEventListener("click", function(){
-    while (toDoContainer.firstChild) {
-        toDoContainer.removeChild(toDoContainer.firstChild);
-    }
+clearBtn.addEventListener("click", function () {
+  while (toDoContainer.firstChild) {
+    toDoContainer.removeChild(toDoContainer.firstChild);
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your todo has been deleted.",
+          icon: "warning",
+        });
+      }
+    });
+  }
 });
 
-
-
-
-
-
-
-
-
-
+inputText.addEventListener("click", function () {
+  if (inputText.value === null || inputText.value === "") {
+    inputText.classList.add("error-border");
+  }
+});
